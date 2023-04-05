0c7263b66ec8f8d15ffeb6b69bac9a977583ebc4
Szymon <113582467+szymon02913@users.noreply.github.com>
2023-04-05 15:30:42 +0200

    Add files via upload

---
+++ wedkuj.php
@@ -0,0 +1,46 @@
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <title>Wędkowanie</title>
+    <link rel="stylesheet" href="styl_1.css">
+    <body>
+    <div id="baner">
+    <h1>Portal dla wędkarzy</h1>    
+    </div>
+    <div id="lewy">
+    <div id="lewy1">
+        <h3>Ryby zamieszkujące rzeki</h3>
+        <ol>
+        
+        
+        </ol>
+        </div>
+        <div id="lewy2">
+        <h3>Ryby drapieżne naszych wód</h3>
+        <table>
+        <tr>
+          <th>L.P</th>
+          <th>Gatunek</th>
+          <th>Występowanie</th>
+         </tr>
+         
+
+
+         
+        </table>
+        </div>
+        </div>
+        <div id="prawy">
+        <img src="ryba1.jpg"Sum>
+        <p><a href="kwerenty1.txt">Pobierz kwerenty</a></p>
+        
+        </div>
+        <div id="stopka">
+        <p>Strone wykonał:Szymon:00000000</p>
+        </div>
+    
+    
+    </body>
+    </head>
+
+</html>
